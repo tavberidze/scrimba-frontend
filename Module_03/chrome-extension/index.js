38c217d793249c88e7d6370337e0843f008a9f21
tavberidze <nikoloz.tavberidze@geolab.edu.ge>
2022-10-24 19:50:39 +0400

    fixed save and delete buttons

---
+++ Module_03/chrome-extension/index.js
@@ -0,0 +1,65 @@
+// need an empty array to hold my input links
+// const variables for input element and unordered list element
+// event listeners for save and delete buttons
+// render function which will display <li> with <a> link inside ulElement / refactor to use a parameter
+// need to save inputs to localStorage for page refresh reasons it saves only strings so i have to stringify my array first and then JSON.parse it back to array to display as intended
+// i will need an if statement for this and to check which parameter to render 
+
+let leadsArr = []
+const inputEl = document.querySelector('#input-el')
+const saveBtn = document.querySelector('#save-btn')
+const deleteBtn = document.querySelector('#delete-btn')
+const ulEL = document.querySelector('#ul-el')
+const localStorageData = JSON.parse(localStorage.getItem('leadsArr'))
+
+if (localStorageData) {
+    leadsArr = localStorageData
+    render(leadsArr)
+}
+
+function render(leads) {
+    let listItem = ''
+    for (let i = 0; i < leads.length; i++) {
+        listItem += `
+        <li>
+        <a target ='_blank' href='${leads[i]}'>${leads[i]}</a>
+        </li>
+        `
+    }
+    ulEL.innerHTML = listItem
+}
+
+saveBtn.addEventListener('click', function() {
+    leadsArr.push(inputEl.value)
+    inputEl.value = ''
+    localStorage.setItem('leadsArr', JSON.stringify(leadsArr))
+    render(leadsArr) 
+})
+
+deleteBtn.addEventListener('dblclick', function() {
+    localStorage.clear()
+    leadsArr = []
+    render(leads)
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
