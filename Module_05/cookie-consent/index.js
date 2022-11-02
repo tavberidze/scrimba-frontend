abd269371564b68f7908ec03c907dfa1673f769c
tavberidze <nikoloz.tavberidze@geolab.edu.ge>
2022-11-02 20:41:48 +0400

    add js

--- Module_05/cookie-consent/index.js
+++ Module_05/cookie-consent/index.js
@@ -1,1 +1,4 @@
-addEventListener
+const modal = document.getElementById('modal')
+const modalCloseBtn = document.getElementById('modal-close-btn')
+const consentForm = document.getElementById('consent-form')
+const modalText = document.getElementById('modal-text')
